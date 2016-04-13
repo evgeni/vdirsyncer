6ad072307aeb82bfac8fc35b592345d78c2aad7a
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-04-13 22:02:42 +0200

    Remove obsolete schema

--- tests/storage/servers/radicale/__init__.py
+++ tests/storage/servers/radicale/__init__.py
@@ -11,39 +11,6 @@ import wsgi_intercept
 import wsgi_intercept.requests_intercept
 
 
-RADICALE_SCHEMA = '''
-create table collection (
-       path varchar(200) not null,
-       parent_path varchar(200) references collection (path),
-       primary key (path));
-
-create table item (
-       name varchar(200) not null,
-       tag text not null,
-       collection_path varchar(200) references collection (path),
-       primary key (name));
-
-create table header (
-       name varchar(200) not null,
-       value text not null,
-       collection_path varchar(200) references collection (path),
-       primary key (name, collection_path));
-
-create table line (
-       name text not null,
-       value text not null,
-       item_name varchar(200) references item (name),
-       timestamp bigint not null,
-       primary key (timestamp));
-
-create table property (
-       name varchar(200) not null,
-       value text not null,
-       collection_path varchar(200) references collection (path),
-       primary key (name, collection_path));
-'''.split(';')
-
-
 def do_the_radicale_dance(tmpdir):
     # All of radicale is already global state, the cleanliness of the code and
     # all hope is already lost. This function runs before every test.
